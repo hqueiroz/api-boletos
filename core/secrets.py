908a0617150b861d40c3b81053b16771f45ff9db
hqueiroz <henrique@isasolucoes.com>
2024-11-16 12:22:38 -0300

    Update secrets.py

--- core/secrets.py
+++ core/secrets.py
@@ -1,10 +1,1 @@
-# secrets
-JUMP_HOST = '3.239.107.39'  # Endereço do jump server
-JUMP_USER = 'ec2-user'  # Usuário para login no jump server
-JUMP_KEY = '/mnt/api-boletos/core/rds-connect.pem'  # Caminho para a chave privada do SSH
-JUMP_PORT = 22
-RDS_HOST = 'ii17gxi6nubz4h4.co1m9zbmvxag.us-east-1.rds.amazonaws.com'  # Endpoint do RDS PostgreSQL
-RDS_PORT = 5432  # Porta do PostgreSQL (padrão é 5432)
-RDS_DBNAME = 'synd'  # Nome do banco de dados
-RDS_USER = 'syscdk'  # Usuário do banco de dados
-RDS_PASSWORD = '^iz.GjNb_a=nbozlBP82TYcM,itDG.'  # Senha do banco de dados
+
